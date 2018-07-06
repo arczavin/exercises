54a83dbd5e2fcf4f34abdbfd0909e7b845e7ce53
arczavin <3day@wp.pl>
2018-07-06 12:02:57 +0200

    textarea works

---
+++ View/free.php
@@ -0,0 +1,31 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link type="text/css" rel="stylesheet" href="../Resources/css/free.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  <title>Exercises</title>
+
+</head>
+
+<body>
+
+  <div class="container">
+
+
+    <form action="../View/buttons.php">
+      <textarea placeholder="Nazwa, telefon, mail, dane do faktury:"required></textarea>
+
+      <br><br>
+
+        <input class="button5" type="submit" value="SEND" />
+    </form>
+
+
+  </div>
+
+
+</body>
+</html>
